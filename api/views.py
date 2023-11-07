4680491d15afc929a353d82147c28eff271bf068
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2023-11-07 17:17:46 -0300

    Iniciando o projeto

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def register(request):
+    return Response('Teste')
